e3169c4c35e4d7c6229bee95a70f9233eb8ed9d2
eelbahri <elyes.elbahri77@gmail.com>
2018-02-16 14:04:53 +0100

    fix test

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -7,26 +7,12 @@ use PHPUnit\Framework\TestCase;
 
 final class EmailTest extends TestCase
 {
-    // public function testCanBeCreatedFromValidEmailAddress(): void
-    // {
-    //     $this->assertInstanceOf(
-    //         Email::class,
-    //         Email::fromString('user@example.com')
-    //     );
-    // }
-
-    // public function testCannotBeCreatedFromInvalidEmailAddress(): void
-    // {
-    //     $this->expectException(InvalidArgumentException::class);
-    //
-    //     Email::fromString('invalid');
-    // }
-
-    public function testCanBeUsedAsString(): void
+    public function testCanBeCreatedFromValidEmailAddress(): void
     {
-        $this->assertEquals(
-            'user@example.com',
-            Email::fromString('user@example.com')
+        $email = newEmail();
+        $this->assertInstanceOf(
+            Email::class,
+            'user@example.com'
         );
     }
 }
